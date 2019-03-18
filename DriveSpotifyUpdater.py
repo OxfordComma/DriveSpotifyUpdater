0fc89e6fec06ad68eef0959db81b5eed9677a7d8
Nick Lima <nlima446@gmail.com>
2019-03-18 09:39:37 -0400

    first commit

---
+++ DriveSpotifyUpdater.py
@@ -0,0 +1,84 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import spotipy
+import spotipy.util as util
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
+def main():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds)
+
+    # Call the Drive v3 API
+    # 0B7wQpwvNx4sTUVZKMFFIVFByakE is the Drive id for the 'Guitar' folder
+    # pageSize 1000 to get all of the documents
+    results = service.files().list(q="'0B7wQpwvNx4sTUVZKMFFIVFByakE' in parents", pageSize = 1000).execute()
+    items = results.get('files', [])
+
+    songs = [i['name'] for i in items]
+    songs = list(filter(lambda x: ('DRAFT' and 'HOLIDAY' and 'Copy') not in x, songs))
+    song_uris = []
+
+    username = '%23oxfordcomma'
+    scope = 'playlist-modify-private'
+    token = util.prompt_for_user_token(username, scope, client_id='837bf722fa9d48cf930a5c9dc70c9de0',
+                               client_secret='7e977f199a594527b752e2a1ecebef98', redirect_uri='http://localhost:8888/callback')
+    playlist_uri = '3DsWreismzG8iHjgVQrEyp'
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        sp.trace = False
+
+        current_tracks_in_playlist = [i['track']['uri'] for i in sp.user_playlist_tracks(username, playlist_uri)['items'] if i]
+
+        for song in songs:
+            search_result = sp.search(song.replace(' -', ''))
+            if len(search_result['tracks']['items']) == 0:
+                print(song + ' is missing?')
+
+            else:
+                song_uri = search_result['tracks']['items'][0]['uri']
+                if song_uri not in current_tracks_in_playlist:
+                    song_uris.append(song_uri)
+            print(song)
+
+        results = sp.user_playlist_add_tracks(username, playlist_uri, song_uris)
+        print(results)
+    else:
+        print("Can't get token for", username)
+
+    if not songs:
+        print('No files found.')
+    else:
+        print('Songs:')
+        for song in songs:
+            print(song)
+
+if __name__ == '__main__':
+    main()
